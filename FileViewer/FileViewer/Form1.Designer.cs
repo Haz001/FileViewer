4db616204115d9dd1a230ee4fdc1eaf7d8c16093
Haz <haz002@hazonline.co.uk>
2019-07-08 00:01:58 +0100

    Made Concept
    Made the basic concept, needs to be rewritten

---
+++ FileViewer/FileViewer/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace FileViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.top_pnl = new System.Windows.Forms.FlowLayoutPanel();
+            this.close_btn = new System.Windows.Forms.Button();
+            this.max_btn = new System.Windows.Forms.Button();
+            this.mini_btn = new System.Windows.Forms.Button();
+            this.side_pnl = new System.Windows.Forms.FlowLayoutPanel();
+            this.path_tb = new System.Windows.Forms.TextBox();
+            this.browser_lv = new System.Windows.Forms.ListView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.cmd_btn = new System.Windows.Forms.Button();
+            this.powers_btn = new System.Windows.Forms.Button();
+            this.top_pnl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // top_pnl
+            // 
+            this.top_pnl.Controls.Add(this.close_btn);
+            this.top_pnl.Controls.Add(this.max_btn);
+            this.top_pnl.Controls.Add(this.mini_btn);
+            this.top_pnl.Controls.Add(this.cmd_btn);
+            this.top_pnl.Controls.Add(this.powers_btn);
+            this.top_pnl.Dock = System.Windows.Forms.DockStyle.Top;
+            this.top_pnl.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.top_pnl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.top_pnl.Location = new System.Drawing.Point(0, 0);
+            this.top_pnl.Margin = new System.Windows.Forms.Padding(0, 0, 0, 0);
+            this.top_pnl.Name = "top_pnl";
+            this.top_pnl.Size = new System.Drawing.Size(1239, 32);
+            this.top_pnl.TabIndex = 3;
+            this.top_pnl.Paint += new System.Windows.Forms.PaintEventHandler(this.top_pnl_Paint);
+            this.top_pnl.MouseDown += new System.Windows.Forms.MouseEventHandler(this.top_pnl_MouseDown);
+            this.top_pnl.MouseMove += new System.Windows.Forms.MouseEventHandler(this.top_pnl_MouseMove);
+            // 
+            // close_btn
+            // 
+            this.close_btn.BackColor = System.Drawing.Color.Gray;
+            this.close_btn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.close_btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.close_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.close_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.close_btn.ForeColor = System.Drawing.Color.White;
+            this.close_btn.Location = new System.Drawing.Point(1191, 0);
+            this.close_btn.Margin = new System.Windows.Forms.Padding(0, 0, 0, 0);
+            this.close_btn.Name = "close_btn";
+            this.close_btn.Size = new System.Drawing.Size(48, 32);
+            this.close_btn.TabIndex = 0;
+            this.close_btn.Text = "X";
+            this.close_btn.UseVisualStyleBackColor = false;
+            this.close_btn.Click += new System.EventHandler(this.close_btn_Click);
+            // 
+            // max_btn
+            // 
+            this.max_btn.BackColor = System.Drawing.Color.Gray;
+            this.max_btn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.max_btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(196)))), ((int)(((byte)(196)))));
+            this.max_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.max_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.max_btn.ForeColor = System.Drawing.Color.White;
+            this.max_btn.Location = new System.Drawing.Point(1143, 0);
+            this.max_btn.Margin = new System.Windows.Forms.Padding(0, 0, 0, 0);
+            this.max_btn.Name = "max_btn";
+            this.max_btn.Size = new System.Drawing.Size(48, 32);
+            this.max_btn.TabIndex = 0;
+            this.max_btn.Text = "<->";
+            this.max_btn.UseVisualStyleBackColor = false;
+            this.max_btn.Click += new System.EventHandler(this.max_btn_Click);
+            // 
+            // mini_btn
+            // 
+            this.mini_btn.BackColor = System.Drawing.Color.Gray;
+            this.mini_btn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.mini_btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(196)))), ((int)(((byte)(196)))));
+            this.mini_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.mini_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mini_btn.ForeColor = System.Drawing.Color.White;
+            this.mini_btn.Location = new System.Drawing.Point(1095, 0);
+            this.mini_btn.Margin = new System.Windows.Forms.Padding(0, 0, 0, 0);
+            this.mini_btn.Name = "mini_btn";
+            this.mini_btn.Size = new System.Drawing.Size(48, 32);
+            this.mini_btn.TabIndex = 0;
+            this.mini_btn.Text = "_";
+            this.mini_btn.UseVisualStyleBackColor = false;
+            this.mini_btn.Click += new System.EventHandler(this.mini_btn_Click);
+            // 
+            // side_pnl
+            // 
+            this.side_pnl.Dock = System.Windows.Forms.DockStyle.Left;
+            this.side_pnl.Location = new System.Drawing.Point(0, 32);
+            this.side_pnl.Name = "side_pnl";
+            this.side_pnl.Size = new System.Drawing.Size(200, 520);
+            this.side_pnl.TabIndex = 4;
+            // 
+            // path_tb
+            // 
+            this.path_tb.Dock = System.Windows.Forms.DockStyle.Top;
+            this.path_tb.Location = new System.Drawing.Point(200, 32);
+            this.path_tb.Name = "path_tb";
+            this.path_tb.Size = new System.Drawing.Size(1039, 20);
+            this.path_tb.TabIndex = 5;
+            this.path_tb.KeyDown += new System.Windows.Forms.KeyEventHandler(this.path_tb_KeyDown);
+            // 
+            // browser_lv
+            // 
+            this.browser_lv.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.browser_lv.LargeImageList = this.imageList1;
+            this.browser_lv.Location = new System.Drawing.Point(200, 52);
+            this.browser_lv.Name = "browser_lv";
+            this.browser_lv.Size = new System.Drawing.Size(1039, 500);
+            this.browser_lv.TabIndex = 6;
+            this.browser_lv.UseCompatibleStateImageBehavior = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "doc.png");
+            // 
+            // cmd_btn
+            // 
+            this.cmd_btn.BackColor = System.Drawing.Color.Gray;
+            this.cmd_btn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.cmd_btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(196)))), ((int)(((byte)(196)))));
+            this.cmd_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmd_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmd_btn.ForeColor = System.Drawing.Color.White;
+            this.cmd_btn.Location = new System.Drawing.Point(1047, 0);
+            this.cmd_btn.Margin = new System.Windows.Forms.Padding(0);
+            this.cmd_btn.Name = "cmd_btn";
+            this.cmd_btn.Size = new System.Drawing.Size(48, 32);
+            this.cmd_btn.TabIndex = 1;
+            this.cmd_btn.Text = "CMD";
+            this.cmd_btn.UseVisualStyleBackColor = false;
+            this.cmd_btn.Click += new System.EventHandler(this.cmd_btn_Click);
+            // 
+            // powers_btn
+            // 
+            this.powers_btn.BackColor = System.Drawing.Color.Gray;
+            this.powers_btn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.powers_btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(196)))), ((int)(((byte)(196)))));
+            this.powers_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.powers_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.powers_btn.ForeColor = System.Drawing.Color.White;
+            this.powers_btn.Location = new System.Drawing.Point(999, 0);
+            this.powers_btn.Margin = new System.Windows.Forms.Padding(0);
+            this.powers_btn.Name = "powers_btn";
+            this.powers_btn.Size = new System.Drawing.Size(48, 32);
+            this.powers_btn.TabIndex = 1;
+            this.powers_btn.Text = "PS";
+            this.powers_btn.UseVisualStyleBackColor = false;
+            this.powers_btn.Click += new System.EventHandler(this.powers_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1239, 552);
+            this.Controls.Add(this.browser_lv);
+            this.Controls.Add(this.path_tb);
+            this.Controls.Add(this.side_pnl);
+            this.Controls.Add(this.top_pnl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Move += new System.EventHandler(this.Form1_Move);
+            this.top_pnl.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.FlowLayoutPanel top_pnl;
+        private System.Windows.Forms.Button close_btn;
+        private System.Windows.Forms.Button max_btn;
+        private System.Windows.Forms.Button mini_btn;
+        private System.Windows.Forms.FlowLayoutPanel side_pnl;
+        private System.Windows.Forms.TextBox path_tb;
+        private System.Windows.Forms.ListView browser_lv;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button cmd_btn;
+        private System.Windows.Forms.Button powers_btn;
+    }
+}
+
---
+++ FileViewer/FileViewer/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace FileViewer
+{
+    public partial class Form1 : Form
+    {
+        public Point loc;
+        Graphics g;
+        Graphics g1;
+        Image img;
+        Point pos = new Point(0,0);
+        Image bmp;
+        string path = "C:\\";
+        public Form1()
+        {
+            InitializeComponent();
+            g = side_pnl.CreateGraphics();
+            g1 = top_pnl.CreateGraphics();
+            img = FileViewer.Properties.Resources._20180827_155533;
+            var tmph = img.Size.Height;
+            var tmpyw = img.Size.Width;
+            int h = (int)(((float)Screen.GetBounds(new Point(this.Left, this.Top)).Width / (float)tmpyw)*tmph);
+            int w = (int)Screen.GetBounds(new Point(this.Left, this.Top)).Width ;
+
+            
+            bmp = new Bitmap(Blur(new Bitmap(img, new Size(w/10,h/10) ),4), new Size(w,h));
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            path = System.IO.Directory.GetCurrentDirectory();
+            path_tb.Text = path.Replace("\\", ">");
+            List<string> ts = DirSearch(path);
+            
+            foreach (string s in ts)
+            {
+                
+                browser_lv.Items.Add(Path.GetFileName(s), 0);
+
+            }
+
+        }
+        public static List<String> DirSearch(string sDir)
+        {
+            List<String> files = new List<String>();
+
+            foreach (string f in Directory.GetFiles(sDir))
+            {
+                try
+                {
+                    
+                    files.Add(f);
+                }
+                catch (Exception e)
+                {
+
+                    //System.Windows.Forms.MessageBox.Show("Access denied");
+                }
+
+            }
+            foreach (string d in Directory.GetDirectories(sDir))
+            {
+                try
+                {
+                    //files.AddRange(DirSearch(d));
+                    files.Add(d);
+                }
+                catch (Exception)
+                {
+
+                    //System.Windows.Forms.MessageBox.Show("access denied");
+                }
+
+            }
+            return files;
+
+        }
+
+        private void Form1_Move(object sender, EventArgs e)
+        {
+            pos = new Point(-this.Left, -this.Top);
+            //label1.Text = pos.ToString()+Screen.GetBounds(new Point(this.Left,this.Top)).ToString();
+            g.DrawImage(bmp, pos.X, pos.Y);
+            g1.DrawImage(bmp, pos.X, pos.Y);
+            g1.FillRectangle(new SolidBrush(Color.FromArgb(128, 0, 0, 0)), new Rectangle(0, 0, top_pnl.Width, top_pnl.Height));
+            g1.DrawString("FileViewer", top_pnl.Font,Brushes.White, new Point(0, 0));
+        }
+
+
+        private void test_1_Resize(object sender, EventArgs e)
+        {
+            g = side_pnl.CreateGraphics();
+        }
+        private static Bitmap Blur(Bitmap image, Int32 blurSize)
+        {
+            return Blur(image, new Rectangle(0, 0, image.Width, image.Height), blurSize);
+        }
+
+        private static Bitmap Blur(Bitmap image, Rectangle rectangle, Int32 blurSize)
+        {
+            Bitmap blurred = new Bitmap(image.Width, image.Height);
+
+            // make an exact copy of the bitmap provided
+            using (Graphics graphics = Graphics.FromImage(blurred))
+                graphics.DrawImage(image, new Rectangle(0, 0, image.Width, image.Height),
+                    new Rectangle(0, 0, image.Width, image.Height), GraphicsUnit.Pixel);
+
+            // look at every pixel in the blur rectangle
+            for (int xx = rectangle.X; xx < rectangle.X + rectangle.Width; xx++)
+            {
+                for (int yy = rectangle.Y; yy < rectangle.Y + rectangle.Height; yy++)
+                {
+                    int avgR = 0, avgG = 0, avgB = 0;
+                    int blurPixelCount = 0;
+
+                    // average the color of the red, green and blue for each pixel in the
+                    // blur size while making sure you don't go outside the image bounds
+                    for (int x = xx; (x < xx + blurSize && x < image.Width); x++)
+                    {
+                        for (int y = yy; (y < yy + blurSize && y < image.Height); y++)
+                        {
+                            Color pixel = blurred.GetPixel(x, y);
+
+                            avgR += pixel.R;
+                            avgG += pixel.G;
+                            avgB += pixel.B;
+
+                            blurPixelCount++;
+                        }
+                    }
+
+                    avgR = avgR / blurPixelCount;
+                    avgG = avgG / blurPixelCount;
+                    avgB = avgB / blurPixelCount;
+
+                    // now that we know the average for the blur size, set each pixel to that color
+                    for (int x = xx; x < xx + blurSize && x < image.Width && x < rectangle.Width; x++)
+                        for (int y = yy; y < yy + blurSize && y < image.Height && y < rectangle.Height; y++)
+                            blurred.SetPixel(x, y, Color.FromArgb(avgR, avgG, avgB));
+                }
+            }
+
+            return blurred;
+        }
+
+        private void side_pnl_Resize(object sender, EventArgs e)
+        {
+            g = side_pnl.CreateGraphics();
+            g1 = top_pnl.CreateGraphics();
+        }
+
+        private void side_pnl_SizeChanged(object sender, EventArgs e)
+        {
+            g = side_pnl.CreateGraphics();
+            g1 = top_pnl.CreateGraphics();
+        }
+
+        private void top_pnl_Paint(object sender, PaintEventArgs e)
+        {
+            g.DrawImage(bmp, pos.X, pos.Y);
+            g1.DrawImage(bmp, pos.X, pos.Y);
+            g1.FillRectangle(new SolidBrush(Color.FromArgb(128, 0, 0, 0)), new Rectangle(0, 0, top_pnl.Width, top_pnl.Height));
+            g1.DrawString("FileViewer", top_pnl.Font, Brushes.White, new Point(0, 0));
+        }
+
+        private void top_pnl_MouseDown(object sender, MouseEventArgs e)
+        {
+            loc = e.Location;
+        }
+
+        private void top_pnl_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Location = new Point(this.Location.X + (e.Location.X - loc.X), this.Location.Y + (e.Location.Y - loc.Y));
+            }
+        }
+
+        private void mini_btn_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void max_btn_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState== FormWindowState.Normal)
+            {
+                this.WindowState = FormWindowState.Maximized;
+                max_btn.Text = ">-<";
+            }else
+            {
+                this.WindowState = FormWindowState.Normal;
+                max_btn.Text = "<->";
+            }
+            g = side_pnl.CreateGraphics();
+            g1 = top_pnl.CreateGraphics();
+        }
+
+        private void close_btn_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void path_tb_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyValue == 13)
+            {
+                path = path_tb.Text.Replace(">", "\\");
+                List<string> ts = DirSearch(path);
+
+                foreach (string s in ts)
+                {
+
+                    browser_lv.Items.Add(Path.GetFileName(s), 0);
+
+                }
+            }
+        }
+
+        private void cmd_btn_Click(object sender, EventArgs e)
+        {
+            Process.Start("cmd","/k "+path[0]+path[1]+" && cd "+path);
+        }
+
+        private void powers_btn_Click(object sender, EventArgs e)
+        {
+            Process.Start("powershell", "-command cd " + path+"\npowershell");
+        }
+    }
+}
